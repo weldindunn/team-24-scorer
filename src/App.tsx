b80ebcabdb17b56dbf83b6b2a7fd858090a9d5fc
Weldin Dunn <weldindunn20@gmail.com>
2022-03-22 15:08:36 -0400

    Completed part b

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,8 @@ import "./App.css";
 function App(): JSX.Element {
     return (
         <div className="App">
-            <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript
-            </header>
-            <p>
-                Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload.
-            </p>
+            <header className="App-header">UD CISC275 Team 24 Scorer</header>
+            <p>Welcome to our wonderful counter!</p>
         </div>
     );
 }
